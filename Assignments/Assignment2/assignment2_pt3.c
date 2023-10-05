d0230af430c427a293adb9b80566b3f45fe840c3
Yvonne Aime <yvonneaime0@gmail.com>
2023-10-04 21:13:11 -0400

    parte tres

--- Assignments/Assignment2/assignment2_pt3.c
+++ Assignments/Assignment2/assignment2_pt3.c
@@ -17,18 +17,18 @@ void numgenerator() {
     
     // Checks to see if the random number generated is even.
     if (random_num % 2 == 0)
-        printf("%d is an even number.\n", random_num);
+        printf("\n%d is an even number.\n\n", random_num);
     // Checks to see if the random number generated is odd.
     else
-        printf("%d is an odd number.\n", random_num);
+        printf("\n%d is an odd number.\n\n", random_num);
         
     /* Checks if the generated random number is divisible by 3 
     (with no remainder). */
     if (random_num % 3 == 0)
-        printf("This number: %d is divisible by 3.\n", random_num);
+        printf("This number: %d is divisible by 3.\n\n", random_num);
     // Checks if the generated random number is divisible by 10.
     if (random_num % 10 == 0)
-        printf("This number is divisible by 10.\n", random_num);
+        printf("This number is divisible by 10.\n\n", random_num);
 }
 
 // Driver Code
