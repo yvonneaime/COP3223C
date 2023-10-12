263f381775a2b0e7c834b801f0cd3424fa9f5ab6
Yvonne Aime <yvonneaime0@gmail.com>
2023-10-12 15:54:08 -0400

    hmm

--- Notes/Functions/func.c
+++ Notes/Functions/func.c
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 
-// Return type 
+// Return type - at the top of program (if it wasnt returning a value you could write it at the bottom)
 int Comp_Grade(char grade);
 
 // Parameter - what is being passed is a character
@@ -22,7 +22,7 @@ int main() {
     while(ans == 'Y' || ans == 'y') {
 
         // Get the grade and number of hours of the current class.
-        printf("Enter yout letter grade. \n");
+        printf("Enter your letter grade. \n");
         my_grade = readChar();
         printf("Enter the number of credit hours.\n");
         scanf("%d", &c_hours);
@@ -41,4 +41,38 @@ int main() {
     printf("Your GPA is %.2lf\n", (double)total_points/total_hours);
     
     return 0;
+}
+
+// Pre - condition: Grade must be a 'A', 'B', 'C', 'D', or 'F' (parameters)
+// Post - condition: A corresponding integer from 0 to 4 based on the grade will be returned. (return)
+
+int Comp_Grade(char grade) {
+    // Return appropriate integer for each grade
+    if (grade == 'A')
+        return 4;
+    else if (grade == 'B')
+        return 3;
+    else if (grade == 'C')
+        return 2;
+    else if (grade == 'D')
+        return 1;
+
+    // For all 'F' cases we will ignore and invalid cases.
+    return 0;
+}
+
+// Pre - condition: none
+// Post - condition: returns the 1st character read from the stream that is not a space, tab, or new line character.
+
+char readChar() {
+    char ans;
+    scanf("%c", &ans);
+
+    // Read another charaxter until you get a non-white space.
+    while (ans == ' ' || ans == '\n' || ans == '\t')
+        scanf("%c", &ans);
+    
+    return ans;
+
+
 }
