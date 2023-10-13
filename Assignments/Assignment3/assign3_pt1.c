0806dde2068ce2d22a73df09bfc0295079376152
Yvonne Aime <yvonneaime0@gmail.com>
2023-10-13 09:31:56 -0400

    pt1

--- Assignments/Assignment3/assign3_pt1.c
+++ Assignments/Assignment3/assign3_pt1.c
@@ -9,7 +9,28 @@
 using 10 assignment statements – just make up the values */
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define ARRAY_LENGTH 10
 
 int main() {
+    int i, new_array[ARRAY_LENGTH];
+    new_array[0] = 2;
+    new_array[1] = 24;
+    new_array[2] = 38;
+    new_array[3] = 44;
+    new_array[4] = 66;
+    new_array[5] = 68;
+    new_array[6] = 77;
+    new_array[7] = 86; 
+    new_array[8] = 88; 
+    new_array[9] = 98; 
+
+    printf("\n⋆⭒˚｡⋆ All elements of the array include: ⋆⭒˚｡⋆ \n");
+    for (i = 0; i < ARRAY_LENGTH; i++)  {
+        printf("%d\n", new_array[i]);
+    }
+
     return 0;
 }
--- Assignments/Assignment3/assign3_pt2.c
+++ Assignments/Assignment3/assign3_pt2.c
@@ -2,3 +2,28 @@
 // COP3223
 // October 10, 2023
 // Lecture Assignment 3 - Part 2
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define ARRAY_LENGTH 10
+
+int main() {
+    int i, random_numbers[ARRAY_LENGTH];
+
+    srand(time(0));
+
+    // Fill the array with random numbers from 0 - 99. 
+    for (i = 0; i < ARRAY_LENGTH; i++) {
+        random_numbers[i] = rand()%100;
+
+    }
+    printf("All elements of the array, includes: ");
+    for (i = 0; i < ARRAY_LENGTH; i++) { 
+        printf("\n%d", random_numbers[i]);
+        printf("\n");
+    }
+
+    return 0;
+}
