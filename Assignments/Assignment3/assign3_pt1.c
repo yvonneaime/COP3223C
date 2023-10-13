deae89e57b6d9d3e8720d74628f204456630ba9a
Yvonne Aime <yvonneaime0@gmail.com>
2023-10-13 09:35:30 -0400

    final pt1

--- Assignments/Assignment3/assign3_pt1.c
+++ Assignments/Assignment3/assign3_pt1.c
@@ -4,9 +4,6 @@
 
 // Lecture Assignment 3 - Part 1
 
-// Write a main function that declares an array of 10 int’s. 
-/* Assign each element in the array a value between 1 and 100
-using 10 assignment statements – just make up the values */
 
 #include <stdio.h>
 #include <time.h>
@@ -16,6 +13,7 @@ using 10 assignment statements – just make up the values */
 
 int main() {
     int i, new_array[ARRAY_LENGTH];
+    // Assign each element in the array a value between 1 and 100 (using 10 assignment statements)
     new_array[0] = 2;
     new_array[1] = 24;
     new_array[2] = 38;
@@ -27,10 +25,11 @@ int main() {
     new_array[8] = 88; 
     new_array[9] = 98; 
 
+    // Prints out all elements of the array. (the ten integers that were declared above).
     printf("\n⋆⭒˚｡⋆ All elements of the array include: ⋆⭒˚｡⋆ \n");
     for (i = 0; i < ARRAY_LENGTH; i++)  {
         printf("%d\n", new_array[i]);
     }
-
+    
     return 0;
 }
