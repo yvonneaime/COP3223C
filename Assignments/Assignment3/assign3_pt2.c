2c3981e54ff09f536d0c73027e3b8c0ff40c35b2
Yvonne Aime <yvonneaime0@gmail.com>
2023-10-13 09:52:03 -0400

    pt2 me down

--- Assignments/Assignment3/assign3_pt2.c
+++ Assignments/Assignment3/assign3_pt2.c
@@ -1,27 +1,31 @@
 // Yvonne Aime
 // COP3223
 // October 10, 2023
+
 // Lecture Assignment 3 - Part 2
 
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
-#define ARRAY_LENGTH 10
+#define ARRAY_LENGTH 100
 
 int main() {
-    int i, random_numbers[ARRAY_LENGTH];
+    int i;
+    double decimal_num[ARRAY_LENGTH];
 
     srand(time(0));
 
-    // Fill the array with random numbers from 0 - 99. 
+    // Fill the array with random numbers from 0.50 and 50.0. 
     for (i = 0; i < ARRAY_LENGTH; i++) {
-        random_numbers[i] = rand()%100;
-
+        decimal_num[i] = (double) (rand() % 100 + 1) / 2.0;
     }
-    printf("All elements of the array, includes: ");
+    
+    // Prints out the array.
+    printf("\nAll elements of the array, includes:\n ");
     for (i = 0; i < ARRAY_LENGTH; i++) { 
-        printf("\n%d", random_numbers[i]);
+        // Converts all elements to a double format.
+        printf("\n%.2lf", decimal_num[i]);
         printf("\n");
     }
 
