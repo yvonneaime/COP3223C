e88313054df5574e64ccf0a69a3418f509f65551
Yvonne Aime <yvonneaime0@gmail.com>
2023-11-05 19:09:55 -0500

    pt a

--- Assignments/Assignment4/assign4_pt1.c
+++ Assignments/Assignment4/assign4_pt1.c
@@ -3,15 +3,21 @@
 // October 19, 2023
 // Lecture Assignment 4 - Part A
 
-//A. Write a function that takes an array of ints, and the size of the array – another int. 
-// It also returns a double. Call this one ‘average.’ Return a double that is the average 
-// of the values in the array. Demonstrate that it works by finding the average of an array 
-// with these values {78, 90, 56, 99, 88, 68, 92}
+/* A. Write a function that takes an array of ints, and the size of the array – another int. 
+ It also returns a double. Call this one ‘average.’
+ Return a double that is the average of the values in the array. 
+
+ Demonstrate that it works by finding the average of an array with these values --
+ {78, 90, 56, 99, 88, 68, 92} */
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main() {
-     
+    int newArray[] = {};
+    double average;
+
     return 0;
  }
 
