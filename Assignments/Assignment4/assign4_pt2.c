868b5cb9c747ee5b9112f0e845cc7e94412f1f37
Yvonne Aime <yvonneaime0@gmail.com>
2023-11-05 22:51:38 -0500

    poosh

--- Assignments/Assignment4/assign4_pt2.c
+++ Assignments/Assignment4/assign4_pt2.c
@@ -3,46 +3,45 @@
 // October 19, 2023
 // Lecture Assignment 4 - Part B
 
-/*  Write a function that takes one double parameter, and returns a char. 
-    The parameter represents a grade, and the char represents the corresponding letter grade. 
-
-    If you pass in 90, the char returned will be ‘A’. If you pass
-    in 58.67, the char returned will be an ‘F’ etc. 
-    Use the grading scheme on the syllabus for this course    */
 
 #include <stdio.h>
 
 char grade_calc(double grade);
 
 int main() {
-    double yourGrade;
-    char letterGrade = grade_calc(yourGrade);
+    // Declaring and initializing yourGrade -- (user input).
+    double yourGrade = 0.00;
 
-    printf("\n\nPlease enter your grade. \n");
+    // Prompts the user to enter a grade in a specific format.
+    printf("\n\nPlease enter your grade (in this format: XX.XX) .𖥔 ݁₊  \n\n");
     scanf("%lf",&yourGrade);
 
-
-    printf("This is your letter grade:%c",letterGrade);
+    char letterGrade = grade_calc(yourGrade);
+    printf("\n\n｡･*˚✧｡ This is your letter grade: %c \n\n",letterGrade);
     return 0;
 }
+
 
 char grade_calc(double grade) {    
     char letterGrade;
 
-    if (grade >= 90.00 || grade <= 100.00)
+/*  If you pass in 90, the char returned will be ‘A’. If you pass in
+    58.67, the char returned will be an ‘F’ etc.    
+ */
+    if (grade >= 90.00 && grade <= 100.00)
         letterGrade = 'A';
-    else if (grade >= 80.00 || grade <= 89.99)
+    else if (grade >= 80.00 && grade <= 89.99)
         letterGrade = 'B';
-    else if (grade >= 70.00 || grade <= 79.99)
+    else if (grade >= 70.00 && grade <= 79.99)
         letterGrade = 'C';
-    else if (grade >= 60.00 || grade <= 69.99)
+    else if (grade >= 60.00 && grade <= 69.99)
         letterGrade = 'D';
-    else if  (grade >= 50.00 || grade <= 59.99)
+    else if (grade >= 50.00 && grade <= 59.99)
         letterGrade = 'F';
     else 
-        printf("Outside of Range - Try Again.");
+        // Handles input outside of range. Returns 'X' to the user. 
+        letterGrade = 'X'; 
 
     return letterGrade;
-    
 }
 
---
+++ Assignments/Assignment4/assign4_pt3.c
@@ -0,0 +1,27 @@
+// Yvonne Aime 
+// COP3223
+// November 5, 2023
+// Lecture Assignment 4 - Part C
+
+
+/*
+    Write a function that takes 3 int arguments and 
+    returns the largest of the 3    
+*/
+
+#include <stdio.h>
+
+int numCalc(int a, int b, int c);
+
+
+int main() {
+
+    return 0;
+}
+
+
+// Function that takes 3 int arguments and returns the largest of the 3. 
+int numCalc(int a, int b, int c) {
+
+    return;
+}
