e442308f6c520a6339fd8d01f9e166eb5f6f220e
Yvonne Aime <yvonneaime0@gmail.com>
2023-11-15 11:39:40 -0500

    assignment 5 intro

--- Assignments/Assignment5/assign5_pt1.c
+++ Assignments/Assignment5/assign5_pt1.c
@@ -7,19 +7,47 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-
 int main() {
     // Used for seeding random number (shuffling cards).
     srand(time(NULL));
 
     /*
-        suitsInHand is 4 counters that represents how many hearts, clubs, 
-        diamonds, spades are in the hand. These 4 counters must add up to 5 
-        for a hand of 5 cards. For example if you have 5 hearts in the hand
-        of cards, the array would have the values 5,0,0,0
+        suitsInHand is 4 counters that represents-- how many 
+        hearts, clubs, diamonds, spades are in the hand.
+
+        These 4 counters must add up to 5 for a hand of 5 cards. 
+            For example if you have 5 hearts in the hand
+            of cards, the array would have the values 5,0,0,0
     */
-    int suitsInHand[4], facesInHand[14];
+
+   /*   facesInHand is 13 counters, that represent how many two’s, three’s, 
+        etc. you have in the hand. This must also total 5. 
+            For example, if you have a pair of 3’s, and three Kings’s, 
+            this array would have the values
+                0,2,0,0,0,0,0,0,0,0,3,0
+
+    While dealing a hand of cards, keep a count of the suitsInHand, 
+    and facesInHand. */
+
+   // suitsInHand[] array with all counters set to 0.
+    int suitsInHand[4] = {0};
+    int facesInHand[14] = {0};
+
+    // allCards represents the total number of cards in a hand.
+    int allCards = 0;
+    for (int i = 0; i < 4; ++i) {
+        allCards += suitsInHand[i];
+    }
+
+    if (allCards == 5) {
+        // Print the values of the suitsInHand array.
+        printf("Suits in hand: Hearts=%d, Clubs=%d, Diamonds=%d, Spades=%d\n",
+               suitsInHand[0], suitsInHand[1], suitsInHand[2], suitsInHand[3]);
+    } else {
+        // If the total is not 5, it prints an error message. 
+        printf("Invalid: Total # of cards must be 5.\n");
+    }
+
 
     return 0;
 }
--- Assignments/Assignment5/assign5_pt2.c
+++ Assignments/Assignment5/assign5_pt2.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 
 int main() {
-    
+   
+  
+
     return 0;
 }
